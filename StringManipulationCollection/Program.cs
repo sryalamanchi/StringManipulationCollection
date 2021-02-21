980a27a8d126ce5a3642c531e7a9e49c85a77254
sryalamanchi <sriniyalamanchi@live.com>
2021-02-20 19:55:38 -0500

    initial commit.

--- StringManipulationCollection/Program.cs
+++ StringManipulationCollection/Program.cs
@@ -6,7 +6,33 @@ namespace StringManipulationCollection
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
-		}
-	}
+			string s = superReducedString("aaabccd");
+            Console.WriteLine(s);
+
+        }
+        static string superReducedString(string s)
+        {
+            //inputstring aaabcddd
+            
+            if (s.Trim().Length > 0)
+            {
+                for (int i = 0; i < s.Length - 1; i++)
+                {
+                    if (s[i] == s[i + 1])
+                    {
+                        s = s.Remove(i, 2);
+                        i = -1;
+                    }
+
+                }
+            }
+            if (s.Length == 0)
+            {
+                return "Empty String";
+            }
+            return s;
+
+        }
+
+    }
 }
